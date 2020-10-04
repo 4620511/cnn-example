ad9308dde3ba2607972b525b0d7a8039e3fae0be
Shunta Komatsu <shuntak217@gmail.com>
2020-10-04 23:05:56 +0900

    Normalize

--- cnn_example/data/transforms.py
+++ cnn_example/data/transforms.py
@@ -2,4 +2,10 @@ import torchvision.transforms as T
 
 
 def get_transforms() -> T.Compose:
-    return T.Compose([T.Resize((224, 224)), T.ToTensor()])
+    return T.Compose(
+        [
+            T.Resize((224, 224)),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
